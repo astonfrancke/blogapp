1e8cd3682d52597c111aa1ce45f9739c84e42967
Aston Francke <francke@hotmail.se>
2019-03-27 21:58:49 +0100

    Added functionality to view all posts, show a specific post, create new post

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">{{config('app.name', 'BLOGAPP')}}</a>
+    <a class="navbar-brand" href="/">{{config('app.name', 'BLOGAPP')}}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -16,6 +16,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/services">Services</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/posts">Blog</a>
+        </li>
         {{-- <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
@@ -24,6 +27,11 @@
         <a class="dropdown-item" href="#">Something else here</a>
       </div>
     </li> --}}
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+    <li class="nav-item">
+      <a class="nav-link" href="/posts/create">Create Post</a>
+    </li>
   </ul>
   {{-- <form class="form-inline my-2 my-lg-0">
   <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
