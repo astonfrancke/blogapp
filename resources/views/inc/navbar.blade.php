9fddb24513ec86f529910d8034facc9f008388bb
Aston Francke <francke@hotmail.se>
2019-03-28 09:54:28 +0100

    added auth and user id when creating a post

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -28,14 +28,44 @@
       </div>
     </li> --}}
   </ul>
-  <ul class="nav navbar-nav navbar-right">
-    <li class="nav-item">
-      <a class="nav-link" href="/posts/create">Create Post</a>
+  <!-- Right Side Of Navbar -->
+  <ul class="navbar-nav ml-auto">
+    <!-- Authentication Links -->
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+      </li>
+      @if (Route::has('register'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+      @endif
+    @else
+      <li class="nav-item">
+        <a class="nav-link" href="/posts/create">Create Post</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+          {{ Auth::user()->name }} <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+      </div>
     </li>
-  </ul>
-  {{-- <form class="form-inline my-2 my-lg-0">
-  <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  @endguest
+</ul>
+{{-- <form class="form-inline my-2 my-lg-0">
+<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 </form> --}}
 </div>
 </div>
